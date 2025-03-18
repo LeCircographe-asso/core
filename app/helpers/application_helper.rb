ec7b10e46d5f536360f896653f6ec9fe39b5142d
Sachathp <sacha.courbe1@gmail.com>
2025-03-18 15:49:38 +0100

    add card and card_reverse components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,25 @@ module ApplicationHelper
   def admin_view?
   authenticated? && authorized_roles.include?(Current.user.role)
   end
+
+  def render_card_component(title:, description:, image:, alt_text:, link:, button_text:)
+    render partial: 'shared/card', 
+          locals: { title: title, 
+                    description: description, 
+                    image: image, 
+                    alt_text: alt_text, 
+                    link: link, 
+                    button_text: button_text }
+  end
+
+  def render_card_component_reverse(title:, description:, image:, alt_text:, link:, button_text:)
+    render partial: 'shared/card_reverse', 
+          locals: { title: title, 
+                    description: description, 
+                    image: image, 
+                    alt_text: alt_text, 
+                    link: link, 
+                    button_text: button_text }
+  end
+
 end
