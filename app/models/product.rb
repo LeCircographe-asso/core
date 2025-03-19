9f628fbf2852ff4e76687f91ac69f033498d9ed0
SamuelChauche <samuel.chauche@gmail.com>
2025-03-19 23:21:01 +0100

    i'm happy because of hadrien = issue resolved

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,15 @@
 class Product < ApplicationRecord
   has_many :book_of_entries
   has_many :price_entries
+  has_many :price_catalogs, through: :price_entries
   has_many :product_orders
   has_many :orders, through: :product_orders
 
   after_create :productValidation
 
-  private
+  def current_price
+    prices.order("price_entries.created_at DESC").first
+  end
 
   def productValidation
     if self.product_name.blank?
