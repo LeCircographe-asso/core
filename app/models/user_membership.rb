4663181e905f3c2ab243dcacb752f1b7c5e9c5e1
Wieedze <maxime.moodz@gmail.com>
2025-03-19 12:43:13 +0100

    schema changed totaly user model modify

--- app/models/user_membership.rb
+++ app/models/user_membership.rb
@@ -1,13 +1,5 @@
 class UserMembership < ApplicationRecord
-  belongs_to :user
-  belongs_to :subscription_type
-  belongs_to :payment, optional: true
 
-  # has_many :payments, dependent: :destroy
-  has_many :training_attendees
-  has_many :user_membership_subscriptions
-  has_many :subscription_types, through: :user_membership_subscriptions
-
-
-  validates :user_id, :subscription_type_id, presence: true
-end
+  enum :status, {active: 0, pending: 1, expired: 2, canceled: 3} 
+  
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,173 +10,173 @@
 
 # En suivant l'ordre des dépendances
 
-EventAttendee.destroy_all
-UserRole.destroy_all
-Donation.destroy_all
-Session.destroy_all
-UserMembership.destroy_all
-Event.destroy_all
-Payment.destroy_all
-SubscriptionType.destroy_all
-Role.destroy_all
-User.destroy_all
+# EventAttendee.destroy_all
+# UserRole.destroy_all
+# Donation.destroy_all
+# Session.destroy_all
+# UserMembership.destroy_all
+# Event.destroy_all
+# Payment.destroy_all
+# SubscriptionType.destroy_all
+# Role.destroy_all
+# User.destroy_all
 
 
 
 
-Role.create(name: 'guest')
-Role.create(name: 'membership')
-Role.create(name: 'circus_membership')
-Role.create(name: 'volunteer')
-Role.create(name: 'admin')
-Role.create(name: 'godmode')
+# Role.create(name: 'guest')
+# Role.create(name: 'membership')
+# Role.create(name: 'circus_membership')
+# Role.create(name: 'volunteer')
+# Role.create(name: 'admin')
+# Role.create(name: 'godmode')
 
-SubscriptionType.create(
-  name: "daily",
-  duration: 1,
-  price: 4,
-  description: "",
-)
+# SubscriptionType.create(
+#   name: "daily",
+#   duration: 1,
+#   price: 4,
+#   description: "",
+# )
 
-SubscriptionType.create(
-  name: "trimestrial",
-  duration: 90,
-  price: 65,
-  description: "",
-)
+# SubscriptionType.create(
+#   name: "trimestrial",
+#   duration: 90,
+#   price: 65,
+#   description: "",
+# )
 
-SubscriptionType.create(
-  name: "annual",
-  duration: 365,
-  price: 150,
-  description: "",
-)
+# SubscriptionType.create(
+#   name: "annual",
+#   duration: 365,
+#   price: 150,
+#   description: "",
+# )
 
-SubscriptionType.create(
-  name: "booklet",
-  duration: -1,
-  price: 30,
-  description: "",
-)
-
-
-
-
-User.create!(
-  email_address: "test@example.com",
-  password: "123456",
-  last_name: "Dupont",
-  first_name: "Jean",
-  birthdate: "1970-01-01",
-  zip_code: "75000",
-  town: "Paris",
-  country: "France",
-  phone_number: "0123456789",
-  occupation: "Developer",
-  specialty: "Backend",
-  image_rights: true,
-  newsletter: false,
-  get_involved: true
-)
-
-User.create!(
-  email_address: "guest@rails.com",
-  password: "123456",
-  role: "guest"
-)
-
-User.create!(
-  email_address: "member@rails.com",
-  password: "123456",
-  role: "membership"
-)
-
-User.create!(
-  email_address: "circus_member@rails.com",
-  password: "123456",
-  role: "circus_membership"
-)
-
-User.create!(
-  email_address: "volunteer@rails.com",
-  password: "123456",
-  role: "volunteer"
-)
-
-User.create!(
-  email_address: "admin@rails.com",
-  password: "123456",
-  role: "admin"
-)
-
-User.create!(
-  email_address: "godmode@rails.com",
-  password: "123456",
-  role: "godmode"
-)
-
-User.create!(
-  email_address: "alixgeydet@gmail.com",
-  password: "123456",
-  role: "admin"
-)
-
-
-10.times do |i|
-  Payment.create(
-    payment_method: %w[CB check cash].sample,
-    amount: rand(100),
-    status: true,
-    user: User.all.sample
-  )
-end
+# SubscriptionType.create(
+#   name: "booklet",
+#   duration: -1,
+#   price: 30,
+#   description: "",
+# )
 
 
 
 
-20.times do
-  User.create!(
-    email_address: Faker::Internet.email,
-    password: "123456",
-    last_name: Faker::Name.last_name,
-    first_name: Faker::Name.first_name,
-    birthdate: Faker::Date.birthday(min_age: 18, max_age: 99),
-    address: Faker::Address.street_address,
-    zip_code: Faker::Address.zip_code,
-    town: Faker::Address.city,
-    country: Faker::Address.country,
-    phone_number: Faker::PhoneNumber.phone_number,
-    occupation: Faker::Job.title,
-    specialty: Faker::Job.field,
-    image_rights: [ true, false ].sample,
-    newsletter: [ true, false ].sample,
-    get_involved: [ true, false ].sample,
-    role: Role.all.sample.name
-  )
-end
+# User.create!(
+#   email_address: "test@example.com",
+#   password: "123456",
+#   last_name: "Dupont",
+#   first_name: "Jean",
+#   birthdate: "1970-01-01",
+#   zip_code: "75000",
+#   town: "Paris",
+#   country: "France",
+#   phone_number: "0123456789",
+#   occupation: "Developer",
+#   specialty: "Backend",
+#   image_rights: true,
+#   newsletter: false,
+#   get_involved: true
+# )
 
-5.times do
-  event = Event.create!(
-    title: Faker::TvShows::BigBangTheory.character,
-    upper_description: "",
-    middle_description: Faker::TvShows::BigBangTheory.quote,
-    bottom_description: "",
-    location: Faker::Address.city,
-    date: Faker::Date.forward(days: 1),
-    creator: User.all.sample
-  )
+# User.create!(
+#   email_address: "guest@rails.com",
+#   password: "123456",
+#   role: "guest"
+# )
 
-  rand(1..5).times do
-    user = User.all.sample
-    unless event.users.include?(user) || event.creator == user
-      EventAttendee.create!(
-        user: user,
-        event: event,
-        payment: Payment.create!(user: user, amount: rand(10..50), status: true, payment_method: [ 'CB', 'check', 'cash' ].sample)
-      )
-    end
-  end
-end
+# User.create!(
+#   email_address: "member@rails.com",
+#   password: "123456",
+#   role: "membership"
+# )
+
+# User.create!(
+#   email_address: "circus_member@rails.com",
+#   password: "123456",
+#   role: "circus_membership"
+# )
+
+# User.create!(
+#   email_address: "volunteer@rails.com",
+#   password: "123456",
+#   role: "volunteer"
+# )
+
+# User.create!(
+#   email_address: "admin@rails.com",
+#   password: "123456",
+#   role: "admin"
+# )
+
+# User.create!(
+#   email_address: "godmode@rails.com",
+#   password: "123456",
+#   role: "godmode"
+# )
+
+# User.create!(
+#   email_address: "alixgeydet@gmail.com",
+#   password: "123456",
+#   role: "admin"
+# )
+
+
+# 10.times do |i|
+#   Payment.create(
+#     payment_method: %w[CB check cash].sample,
+#     amount: rand(100),
+#     status: true,
+#     user: User.all.sample
+#   )
+# end
+
+
+
+
+# 20.times do
+#   User.create!(
+#     email_address: Faker::Internet.email,
+#     password: "123456",
+#     last_name: Faker::Name.last_name,
+#     first_name: Faker::Name.first_name,
+#     birthdate: Faker::Date.birthday(min_age: 18, max_age: 99),
+#     address: Faker::Address.street_address,
+#     zip_code: Faker::Address.zip_code,
+#     town: Faker::Address.city,
+#     country: Faker::Address.country,
+#     phone_number: Faker::PhoneNumber.phone_number,
+#     occupation: Faker::Job.title,
+#     specialty: Faker::Job.field,
+#     image_rights: [ true, false ].sample,
+#     newsletter: [ true, false ].sample,
+#     get_involved: [ true, false ].sample,
+#     role: Role.all.sample.name
+#   )
+# end
+
+# 5.times do
+#   event = Event.create!(
+#     title: Faker::TvShows::BigBangTheory.character,
+#     upper_description: "",
+#     middle_description: Faker::TvShows::BigBangTheory.quote,
+#     bottom_description: "",
+#     location: Faker::Address.city,
+#     date: Faker::Date.forward(days: 1),
+#     creator: User.all.sample
+#   )
+
+#   rand(1..5).times do
+#     user = User.all.sample
+#     unless event.users.include?(user) || event.creator == user
+#       EventAttendee.create!(
+#         user: user,
+#         event: event,
+#         payment: Payment.create!(user: user, amount: rand(10..50), status: true, payment_method: [ 'CB', 'check', 'cash' ].sample)
+#       )
+#     end
+#   end
+# end
 
 
 # 10.times do
