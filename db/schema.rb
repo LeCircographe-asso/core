b7ff6edf85b9e9f2ffd37bd396bef394de6415d8
SamuelChauche <samuel.chauche@gmail.com>
2025-03-19 17:16:01 +0100

    Creation of product database and models related

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_15_151614) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_19_155700) do
+  create_table "book_of_entries", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "user_id", null: false
+    t.integer "remaining"
+    t.integer "total_entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_book_of_entries_on_product_id"
+    t.index ["user_id"], name: "index_book_of_entries_on_user_id"
+  end
+
   create_table "donations", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "payment_id", null: false
@@ -46,6 +57,16 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_15_151614) do
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.decimal "sum"
+    t.date "date"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "product_order_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "payments", force: :cascade do |t|
     t.string "payment_method"
     t.decimal "amount"
@@ -54,6 +75,37 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_15_151614) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_payments_on_user_id"
+  end
+
+  create_table "price_catalogs", force: :cascade do |t|
+    t.boolean "active"
+    t.decimal "price", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "price_entries", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "price_catalog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["price_catalog_id"], name: "index_price_entries_on_price_catalog_id"
+    t.index ["product_id"], name: "index_price_entries_on_product_id"
+  end
+
+  create_table "product_orders", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_product_orders_on_order_id"
+    t.index ["product_id"], name: "index_product_orders_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "product_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
@@ -146,13 +198,21 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_15_151614) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "book_of_entries", "products"
+  add_foreign_key "book_of_entries", "users"
   add_foreign_key "donations", "payments"
   add_foreign_key "donations", "users"
   add_foreign_key "event_attendees", "events"
   add_foreign_key "event_attendees", "payments"
   add_foreign_key "event_attendees", "users"
   add_foreign_key "events", "users", column: "creator_id"
+  add_foreign_key "orders", "product_orders"
+  add_foreign_key "orders", "users"
   add_foreign_key "payments", "users"
+  add_foreign_key "price_entries", "price_catalogs"
+  add_foreign_key "price_entries", "products"
+  add_foreign_key "product_orders", "orders"
+  add_foreign_key "product_orders", "products"
   add_foreign_key "sessions", "users"
   add_foreign_key "training_attendees", "user_memberships"
   add_foreign_key "training_attendees", "users"
