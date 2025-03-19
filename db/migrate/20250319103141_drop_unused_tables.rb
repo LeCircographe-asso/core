3f5a0a56e49d3fc1df0408c3cb15525689296346
Wieedze <maxime.moodz@gmail.com>
2025-03-19 12:43:13 +0100

    schema changed totaly user model modify

---
+++ db/migrate/20250319103141_drop_unused_tables.rb
@@ -0,0 +1,13 @@
+class DropUnusedTables < ActiveRecord::Migration[8.0]
+  def change
+    drop_table :donations
+    drop_table :user_memberships
+    drop_table :event_attendees
+    drop_table :payments
+    drop_table :roles
+    drop_table :subscription_types
+    drop_table :training_attendees
+    drop_table :user_membership_subscriptions
+    drop_table :user_roles
+  end
+end
