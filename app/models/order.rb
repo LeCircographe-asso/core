c85ba7986583f4e3486dbd4dde452bde0c6f9d05
Wieedze <maxime.moodz@gmail.com>
2025-03-20 11:38:58 +0100

    update membership link to  payment order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,13 +4,13 @@ class Order < ApplicationRecord
   has_many :products, through: :product_orders
   validates :user_id, presence: true
 
-  # after_create :sumValidation
+#   after_create :sumValidation
 
-  # def sumValidation
-  #   if self.sum <= 0
-  #     Rails.logger.error("Le panier #{self.id} est vide !")
-  #   end
-  # end
+  
 
-
-end
+#   def sumValidation
+#     if self.sum <= 0
+#       Rails.logger.error("Le panier #{self.id} est vide !")
+#     end
+#   end
+# end
