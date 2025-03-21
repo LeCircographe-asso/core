eb6560ec3c013ce3f88df0e7c1f3c82dc4768df4
Wieedze <maxime.moodz@gmail.com>
2025-03-21 18:35:27 +0100

    'Refacto (Payement + Membership) first try to assign membership after payment in the payment model + rebase commit (clean git graph)

--- app/controllers/admin/membership_controller.rb
+++ app/controllers/admin/membership_controller.rb
@@ -1,15 +1,31 @@
-class Admin::MembershipController < ApplicationController
+module Admin
+class MembershipController < BaseController
+
+  before_action :set_products, only: [:index]
+
 
   def index
-    @membership = Membership.all
+    @user = User.find(params[:user_id])
+    @orders = @user.orders.includes(:product_orders)
+    @products = Product.all
   end
 
-  def show
-    @membership = Memberships.find(params[:id])
-  end
 
   def create
-    @membership = Membership.create 
-  end 
+      @user = User.find(params[:user_id]) 
+      @product = Product.find(params[:product_id])  
+      # Ajoutez le produit à la commande
+      @order.product_orders.create(product: @product)
+  
+      redirect_to admin_membership_index_path(user_id: @user.id), notice: "Produit ajouté à la cotisation."
+  end
 
+
+  private
+
+  def set_products
+    @products = Product.all
+  end
+
+end
 end
