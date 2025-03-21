acc028c12d1a486823b7453259ce3d0ce832607e
AkaKwak <christophe.alaterre@gmail.com>
2025-03-22 00:42:59 +0100

    Enhance asset management and update navbar links
    - Updated the asset manifest to include additional stylesheets and images for better resource management.
    - Implemented a global initialization function for Flowbite components to ensure proper functionality across the application.
    - Modified the navbar links to point to the correct "Blog & Newsletters" page, improving navigation accuracy.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,1 +1,6 @@
-//= link favicon/site.webmanifest//= link_tree ../builds
+//= link favicon/site.webmanifest
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../builds
+//= link flowbite.css
+//= link app.css
