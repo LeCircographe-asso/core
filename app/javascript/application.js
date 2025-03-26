8e9d4ede88805ec03db23d55f36fb90d3466ee73
Sachathp <sacha.courbe1@gmail.com>
2025-03-26 17:09:52 +0100

    refacto js home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "flowbite"
+import "home_animations"
 
 // Définir la fonction initFlowbite si elle n'existe pas déjà
 if (typeof window.initFlowbite !== 'function') {
