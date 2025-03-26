ce17fc7d6db7238d972deaa4eb063157b5e72183
SamuelChauche <samuel.chauche@gmail.com>
2025-03-26 16:57:18 +0100

    unsubscribed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,6 +11,7 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = User.find_by(params[:id])
     @url = "http://lecircographe.fr"
+    @unsubscribe_url = url_for(controller: 'users', action: 'change_newsletter_status', token: @user.unsubscribe_token)
     mail(to: @user.email_address, subject: "Bienvenue au Circographe ! 🎉")
   end
 
