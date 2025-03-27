5bb02db445a02fc3f370cdbea380b7acc0fae5ad
SamuelChauche <samuel.chauche@gmail.com>
2025-03-27 19:26:38 +0100

    test admin mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,16 +4,15 @@ class UserMailer < ApplicationMailer
   def welcome_by_admin(user, reset_password_url)
     @user = user
     @reset_password_url = reset_password_url
-    @url = "http://lecircographe.fr"
+    @url = "https://lecircographe.fr"
     mail(to: @user.email_address, subject: "Bienvenue au Circographe ! 🎉")
   end
 
   def welcome_email(user)
     @user = user
-    @url = "http://lecircographe.fr"
+    @url = "https://lecircographe.fr"
     mail(to: @user.email_address, subject: "Bienvenue au Circographe ! 🎉")
   end
-
 
   def contact_email(name, email, message, category, recipient_email)
     @name = name
