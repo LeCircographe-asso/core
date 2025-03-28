7a783f0dcc078b653980832c0f14b10974435d91
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-03-28 13:24:13 +0100

    fix routes basic crud blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,19 +2,20 @@ class BlogsController < ApplicationController
 
   before_action :set_blog, only: %i[show ]
 
-  def article
-  end
+  # def article
+  # end
 
-  def newsletter
-  end
+  # def newsletter
+  # end
+
+  
 
 
-  # GET /blogs or /blogs.json
+
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1 or /blogs/1.json
   def show
     @tags=@blog.tags
   end
