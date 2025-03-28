fe9daaa2be7fb4f2e7499f9182cac944f5578d2a
SamuelChauche <samuel.chauche@gmail.com>
2025-03-28 12:23:22 +0100

    Create membership expiration date mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,17 +58,19 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "htttps://lecircographe.fr" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  user_name => ENV['MAILJET_LOGIN'],
+  password => ENV['MAILJET_PWD'],
+  domain => 'monsite.fr',
+  address => 'in-v3.mailjet.com',
+  port => 587,
+  authentication => :plain,
+  enable_starttls_auto: true
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
