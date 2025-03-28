466446e56961505f76fda2a3614131f877950dc8
Wieedze <maxime.moodz@gmail.com>
2025-03-28 17:38:11 +0100

    donation is working

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -23,8 +23,7 @@ module Admin
         product_order.product.price_entries.order(created_at: :desc).first&.price_catalog&.price.to_i
       end
       
-      @total_donation = @order.donation || 0
-      
+      @total_donation = @order.donation 
       @total_payment = @total_amount + @total_donation
 
     end 
