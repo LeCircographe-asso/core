6c8e0bc5e2632042c48ca27491bd7396cbb781fe
AkaKwak <christophe.alaterre@gmail.com>
2025-04-17 16:43:57 +0200

    Configure RSpec as the test framework and set up FactoryBot for fixture replacement in the application. Remove legacy test files and fixtures to streamline the test suite.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,10 @@ module Circographe
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     # config.action_mailer.default_url_options = { host: "circographe.com" }
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
--- test/models/book_of_entry_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class BookOfEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
