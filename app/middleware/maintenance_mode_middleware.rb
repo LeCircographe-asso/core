38e6a4cb8a0c0668553580d83365104efc36a993
AkaKwak <christophe.alaterre.com>
2025-09-30 16:25:42 +0200

    chore: add Retry-After to maintenance response (production-friendly)

--- app/middleware/maintenance_mode_middleware.rb
+++ app/middleware/maintenance_mode_middleware.rb
@@ -48,7 +48,8 @@ class MaintenanceModeMiddleware
 
     headers = {
       "Content-Type" => "text/html; charset=utf-8",
-      "Cache-Control" => "no-store, no-cache, must-revalidate, max-age=0"
+      "Cache-Control" => "no-store, no-cache, must-revalidate, max-age=0",
+      "Retry-After" => "300"
     }
 
     [503, headers, [body]]
