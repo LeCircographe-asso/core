f33726ebe10e2c63c564d37f59e050e3446122f1
AkaKwak <christophe.alaterre.com>
2025-10-20 05:14:20 +0200

    feat: Add event and attendance systems

---
+++ db/migrate/20251018182934_create_event_system.rb
@@ -0,0 +1,40 @@
+class CreateEventSystem < ActiveRecord::Migration[8.0]
+  def change
+    # Event System
+    create_table "events", force: :cascade do |t|
+      t.string "name", null: false
+      t.text "upper_description"
+      t.text "middle_description"
+      t.text "bottom_description"
+      t.string "location"
+      t.datetime "date", null: false
+      t.bigint "creator_id", null: false
+      t.string "picture_url"
+      t.integer "category", default: 0
+      t.text "description"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["category"], name: "index_events_on_category"
+      t.index ["creator_id"], name: "index_events_on_creator_id"
+      t.index ["date"], name: "index_events_on_date"
+    end
+
+    create_table "event_attendees", force: :cascade do |t|
+      t.bigint "user_id", null: false
+      t.bigint "event_id", null: false
+      t.bigint "payment_id"
+      t.boolean "interested", default: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["event_id"], name: "index_event_attendees_on_event_id"
+      t.index ["payment_id"], name: "index_event_attendees_on_payment_id"
+      t.index ["user_id"], name: "index_event_attendees_on_user_id"
+    end
+
+    # Foreign Keys
+    add_foreign_key "events", "users", column: "creator_id"
+    add_foreign_key "event_attendees", "events"
+    add_foreign_key "event_attendees", "users"
+    add_foreign_key "event_attendees", "payments"
+  end
+end
---
+++ db/migrate/20251018183005_create_attendance_system.rb
@@ -0,0 +1,29 @@
+class CreateAttendanceSystem < ActiveRecord::Migration[8.0]
+  def change
+    # Attendance System
+    create_table "attendance_lists", force: :cascade do |t|
+      t.datetime "start_date"
+      t.datetime "end_date"
+      t.integer "list_type"
+      t.integer "status"
+      t.string "name"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+
+    create_table "attendances", force: :cascade do |t|
+      t.bigint "person_id", null: false
+      t.bigint "event_id"
+      t.date "date", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["person_id", "date"], name: "index_attendances_on_person_id_and_date", unique: true
+      t.index ["person_id"], name: "index_attendances_on_person_id"
+      t.index ["event_id"], name: "index_attendances_on_event_id"
+    end
+
+    # Foreign Keys
+    add_foreign_key "attendances", "people"
+    add_foreign_key "attendances", "events"
+  end
+end
