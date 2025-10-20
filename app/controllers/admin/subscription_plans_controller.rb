249280b7e418fc4bff839fea36888f273a38b7bc
AkaKwak <christophe.alaterre.com>
2025-10-20 05:11:44 +0200

    feat: Add subscription plans management

--- app/controllers/admin/subscription_plans_controller.rb
+++ app/controllers/admin/subscription_plans_controller.rb
@@ -1,15 +1,15 @@
 module Admin
   class SubscriptionPlansController < BaseController
-    before_action :set_person, only: [:show, :new, :create]
+    before_action :set_subscription_plan, only: [:show, :edit, :update, :destroy]
+    before_action :set_person, only: [:new, :create]
     before_action :set_breadcrumbs
 
     def index
-      @subscription_plans = SubscriptionPlan.includes(:membership_type).all
+      @subscription_plans = SubscriptionPlan.includes(:membership_type).all.order(:duration, :price_cents)
       add_breadcrumb "Plans d'Abonnement", nil
     end
 
     def show
-      @subscription_plan = SubscriptionPlan.find(params[:id])
       @book_of_entries = @subscription_plan.book_of_entries.includes(:person)
       add_breadcrumb "Plan: #{@subscription_plan.name}", nil
     end
@@ -26,6 +26,27 @@ module Admin
       end
       
       add_breadcrumb "Nouvel abonnement", nil
+    end
+
+    def edit
+      add_breadcrumb "Modifier: #{@subscription_plan.name}", nil
+    end
+
+    def update
+      if @subscription_plan.update(subscription_plan_params)
+        redirect_to admin_subscription_plan_path(@subscription_plan), notice: "Plan d'abonnement mis à jour avec succès !"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      if @subscription_plan.book_of_entries.any?
+        redirect_to admin_subscription_plans_path, alert: "Impossible de supprimer ce plan car il est utilisé par des carnets d'entrées."
+      else
+        @subscription_plan.destroy
+        redirect_to admin_subscription_plans_path, notice: "Plan d'abonnement supprimé avec succès !"
+      end
     end
 
     def create
@@ -59,7 +80,7 @@ module Admin
         description: "Plan d'abonnement #{@subscription_plan.name}"
       )
 
-      redirect_to admin_subscription_plan_path(@subscription_plan), notice: "Plan d'abonnement acheté avec succès"
+      redirect_to admin_user_path("person_#{@person.id}"), notice: "Cotisation achetée avec succès !"
     rescue => e
       flash[:alert] = "Erreur lors de l'achat du plan: #{e.message}"
       redirect_to new_admin_subscription_plan_path(person_id: @person.id)
@@ -67,16 +88,25 @@ module Admin
 
     private
 
+    def set_subscription_plan
+      @subscription_plan = SubscriptionPlan.find(params[:id])
+    end
+
     def set_person
       @person = Person.find(params[:person_id]) if params[:person_id]
     end
 
     def set_breadcrumbs
-      add_breadcrumb "Administration", admin_root_path
+      add_breadcrumb "Administration", admin_dashboard_index_path
+      add_breadcrumb "Plans d'Abonnement", admin_subscription_plans_path
     end
 
     def subscription_params
       params.require(:subscription_plan).permit(:person_id, :subscription_plan_id, :payment_method)
+    end
+
+    def subscription_plan_params
+      params.require(:subscription_plan).permit(:name, :duration, :price_cents, :description, :membership_type_id, :sessions_count, :validity_days)
     end
   end
 end
