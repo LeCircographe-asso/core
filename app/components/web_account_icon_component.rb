2c91c13d79fe6bf4c886209b26bc89a10f9f9b5c
AkaKwak <christophe.alaterre.com>
2025-10-20 18:42:21 +0200

    feat: Add badge View Components for user management
    - MembershipStatusBadgeComponent: Display membership status with colors
    - MembershipTypeBadgeComponent: Show membership type (Basic/Circus)
    - SubscriptionStatusBadgeComponent: Display subscription status
    - WebAccountIconComponent: Show web account availability icon
    - Include both admin/users/ and root versions for compatibility
    
    Composants de visualisation d'état regroupés par fonctionnalité

---
+++ app/components/web_account_icon_component.rb
@@ -0,0 +1,42 @@
+class WebAccountIconComponent < ViewComponent::Base
+  def initialize(person:)
+    @person = person
+  end
+
+  private
+
+  attr_reader :person
+
+  def icon_class
+    if person.user
+      "h-5 w-5 text-green-500 cursor-help"
+    else
+      "h-5 w-5 text-gray-400 cursor-help"
+    end
+  end
+
+  def icon_title
+    if person.user
+      "Compte web actif (#{person.user.system_role.humanize})"
+    else
+      "Pas de compte web"
+    end
+  end
+
+  def tooltip_data
+    {
+      controller: "tooltip",
+      tooltip_content: icon_title
+    }
+  end
+
+  def svg_path
+    if person.user
+      # Check circle icon
+      "M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+    else
+      # X circle icon
+      "M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+    end
+  end
+end
