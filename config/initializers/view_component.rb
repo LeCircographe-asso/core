94ac6146ca32d8743eb7be16bb1c9606f909a715
AkaKwak <christophe.alaterre.com>
2025-10-30 23:34:23 +0100

    Remove manual autoload_paths addition to prevent frozen array error in Rails 8.1

--- config/initializers/view_component.rb
+++ config/initializers/view_component.rb
@@ -2,8 +2,4 @@
 Rails.application.configure do
   # Configuration ViewComponent
   config.view_component.view_component_path = "app/components"
-  
-  # Forcer l'autoloading des components
-  config.autoload_paths += %W(#{config.root}/app/components)
-  config.eager_load_paths += %W(#{config.root}/app/components)
 end
