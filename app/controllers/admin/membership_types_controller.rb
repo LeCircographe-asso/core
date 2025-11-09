def46b48dcae3a99f3ad362f0a61a6da9ade9862
AkaKwak <christophe.alaterre.com>
2025-11-09 02:06:55 +0100

    refactor(admin): inline CRUD for membership types, remove *Management calls

--- app/controllers/admin/membership_types_controller.rb
+++ app/controllers/admin/membership_types_controller.rb
@@ -23,26 +23,15 @@ module Admin
     end
 
     def create
-      creator = MembershipTypeManagement::MembershipTypeCreator.new(
-        name: membership_type_params[:name],
-        category: membership_type_params[:category],
-        price_cents: membership_type_params[:price_cents],
-        description: membership_type_params[:description],
-        effective_from: membership_type_params[:effective_from] || Date.current,
-        version: membership_type_params[:version] || 1,
-        created_by_user_id: Current.user.id
-      )
+      @membership_type = MembershipType.new(membership_type_params.merge(created_by_user: Current.user))
 
-      result = creator.call
-
-      if result.success?
+      if @membership_type.save
         respond_to do |format|
           format.html { redirect_to admin_membership_types_path, notice: "Type d'adhésion créé avec succès !" }
           format.turbo_stream { redirect_to admin_membership_types_path, notice: "Type d'adhésion créé avec succès !" }
         end
       else
-        @membership_type = MembershipType.new(membership_type_params)
-        flash.now[:alert] = result.message
+        flash.now[:alert] = @membership_type.errors.full_messages.to_sentence
         render :new, status: :unprocessable_content
       end
     end
@@ -52,41 +41,22 @@ module Admin
     end
 
     def update
-      updater = MembershipTypeManagement::MembershipTypeUpdater.new(
-        membership_type_id: @membership_type.id,
-        name: membership_type_params[:name],
-        category: membership_type_params[:category],
-        price_cents: membership_type_params[:price_cents],
-        description: membership_type_params[:description],
-        effective_from: membership_type_params[:effective_from],
-        updated_by_id: Current.user.id
-      )
-
-      result = updater.call
-
-      if result.success?
+      if @membership_type.update(membership_type_params)
         respond_to do |format|
           format.html { redirect_to admin_membership_types_path, notice: "Type d'adhésion mis à jour avec succès !" }
           format.turbo_stream { redirect_to admin_membership_types_path, notice: "Type d'adhésion mis à jour avec succès !" }
         end
       else
-        flash.now[:alert] = result.message
+        flash.now[:alert] = @membership_type.errors.full_messages.to_sentence
         render :edit, status: :unprocessable_content
       end
     end
 
     def destroy
-      deleter = MembershipTypeManagement::MembershipTypeDeleter.new(
-        membership_type_id: @membership_type.id,
-        deleted_by_id: Current.user.id
-      )
-
-      result = deleter.call
-
-      if result.success?
+      if @membership_type.destroy
         redirect_to admin_membership_types_path, notice: "Type d'adhésion supprimé avec succès !"
       else
-        redirect_to admin_membership_types_path, alert: result.message
+        redirect_to admin_membership_types_path, alert: @membership_type.errors.full_messages.to_sentence
       end
     end
 
