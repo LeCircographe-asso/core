c218d0b06dc07c7763f2ce2b39101eab0271d449
AkaKwak <christophe.alaterre@gmail.com>
2026-05-02 13:19:02 +0200

    admin(shared): add person context helpers and refresh labels

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -10,6 +10,16 @@ module Admin
 
     private
 
+    def admin_person_path(person)
+      admin_user_path(Admin::Users::PersonRouteKey.call(person))
+    end
+
+    def add_person_context_breadcrumbs(person, current_label = nil)
+      add_breadcrumb I18n.t("breadcrumbs.admin.users.members_list"), admin_users_path
+      add_breadcrumb person.full_name, admin_person_path(person)
+      add_breadcrumb current_label, nil if current_label.present?
+    end
+
     def require_admin_zone_access
       resume_session
 
